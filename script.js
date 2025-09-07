8a84225e6d9ce262a29220219d523e8da043e713
Lucas Ismael <91797867+lucasismaelbr@users.noreply.github.com>
2025-09-07 19:43:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(`Hello World!`)
+
+const n1 = 8;
+const n2 = 8;
+const idade = n1 + n2;
+
+let firstName = `Lucas`;
+let lastName = `Ismael`;
+let fullName = (`${firstName} ${lastName}`);
+
+console.log(`Meu nome é ${fullName} tenho atualmente ${idade} anos`);
